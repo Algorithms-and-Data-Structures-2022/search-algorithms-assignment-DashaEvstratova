58d9355c362328ab40dca258cfeddb5efba3f9f7
DashaEvstratova <alis131140@gmail.com>
2022-04-15 15:05:53 +0300

    1

--- src/binary_search_iterative.cpp
+++ src/binary_search_iterative.cpp
@@ -3,7 +3,22 @@
 namespace assignment {
 
   std::optional<int> BinarySearchIterative::Search(const std::vector<int>& data, int search_element) const {
+    int l = 0;
+    int r = static_cast<int>(data.size()) - 1;
 
+    while (l <= r) {
+      int m = l + (r - l) / 2;
+
+      if (data[m] == search_element) {
+        return m;
+      }
+
+      if (data[m] < search_element) {
+        l = m + 1;
+      } else if (data[m] > search_element) {
+        r = m - 1;
+      }
+    }
     // Tips:
     // 1. Заведите две переменные: (а) индекс левой границы и (б) индекс правой границы.
     // 2. Поиск ведется пока индекс левой границы не превысил индекс правой.
