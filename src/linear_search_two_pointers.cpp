cf371c32d70f4264acf90ce76548038c0a3e89ae
DashaEvstratova <alis131140@gmail.com>
2022-04-21 22:57:49 +0300

    well

--- src/linear_search_two_pointers.cpp
+++ src/linear_search_two_pointers.cpp
@@ -3,7 +3,17 @@
 namespace assignment {
 
   std::optional<int> LinearSearchTwoPointers::Search(const std::vector<int>& data, int search_element) const {
-
+    int left_point = 0, right_point = static_cast<int>(data.size() - 1);
+    while (left_point <= right_point) {
+      if (data[left_point] == search_element) {
+        return left_point;
+      }
+      if (data[right_point] == search_element) {
+        return right_point;
+      }
+      right_point--;
+      left_point++;
+    }
     // Tips:
     // 1. Задайте две переменные: (1) индекс первого элемента и (2) индекс последнего элемента.
     // 2. Проверяйте наличие целевого элемента по индексам.
