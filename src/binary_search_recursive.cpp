8cd41ea8f7deb344345b3bf039edf73e3d3b4fef
DashaEvstratova <alis131140@gmail.com>
2022-04-21 22:08:58 +0300

    6

--- src/binary_search_recursive.cpp
+++ src/binary_search_recursive.cpp
@@ -14,20 +14,25 @@ namespace assignment {
   std::optional<int> BinarySearchRecursive::recursive_helper(const std::vector<int>& data, int search_element,
                                                              int left_index, int right_index) const {
 
-    if (right_index <= left_index){
-      if (right_index < left_index){
+    if (right_index <= left_index) {
+      if (right_index < left_index) {
         return std::nullopt;
       }
-      if (data[left_index] == search_element){
+      if (data[left_index] == search_element) {
         return left_index;
       }
+      if (data[right_index] == search_element) {
+        return right_index;
+      }
+      return std::nullopt;
     }
-    int m = (right_index + left_index) / 2;
-    if (data[m] < search_element) {
-      return recursive_helper(data, search_element, m, right_index);;
-    }
-    else if (data[m] > search_element) {
-      return recursive_helper(data, search_element, left_index, m);;
+    else {
+      int m = (left_index + right_index) / 2;
+      if (data[m] < search_element) {
+        return recursive_helper(data, search_element, m, right_index);
+      } else {
+        return recursive_helper(data, search_element, left_index, m);
+      }
     }
     // Tips:
     // 1. Рассмотрите базовые случаи выхода и рекурсии:
